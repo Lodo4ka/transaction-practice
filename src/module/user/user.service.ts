331c7b6413e69de0caaf03b38cd5fbbfd4c83456
Maxim Novikov <maxim.novikov@onlinesup.com>
2025-06-22 22:57:21 +0300

    emit not found error when user was not found

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async getBalance(id: string): Promise<number | null> {
+  async getBalance(id: string): Promise<number> {
     const userId = parseInt(id);
     const user = await this.userRepository.getUserById(userId);
-    return user?.balance.toNumber() ?? null;
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user.balance.toNumber() ?? 0;
   }
 }
