73b4eb57bd5dcd003bc20053f6e484704e7d1b28
Maxim Novikov <maxim.novikov@onlinesup.com>
2025-06-22 15:38:26 +0300

    change url for docker redis, change test url for check cache

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('cache')
+  @Get('test/cache')
   getHelloWithCache(): Promise<string> {
     return this.appService.getHelloWithCache();
   }
