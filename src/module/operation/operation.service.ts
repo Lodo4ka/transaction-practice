8b8f477194b3a5dd518019a559a3ef0469048c8b
Maxim Novikov <maxim.novikov@onlinesup.com>
2025-06-22 22:59:39 +0300

    add transaction for change amount

--- src/module/operation/operation.service.ts
+++ src/module/operation/operation.service.ts
@@ -1,15 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { OperationRepository } from './operation.repository';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateOperationDto } from './dto/create-operation';
-import { Operation } from '@prisma/client';
+import { Operation, OperationStatus } from '@prisma/client';
+import { PrismaService } from 'nestjs-prisma';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { CacheManagerStore } from 'cache-manager';
 
 @Injectable()
 export class OperationService {
-  constructor(private readonly operationRepository: OperationRepository) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject(CACHE_MANAGER) private cacheManager: CacheManagerStore,
+  ) {}
 
   async createOperation(data: CreateOperationDto): Promise<Operation> {
-    const operation = await this.operationRepository.createOperation(data);
+    const { amount, senderId, receiverId } = data;
 
-    return operation;
+    return this.prisma.$transaction(async (tx) => {
+      const userSender = await tx.user.findUniqueOrThrow({
+        where: { id: senderId },
+      });
+      const receiver = await tx.user.findUniqueOrThrow({
+        where: { id: receiverId },
+      });
+      if (userSender.balance.lessThan(amount)) {
+        throw new BadRequestException('Insufficient balance');
+      }
+
+      const updatedUserSender = await tx.user.update({
+        where: { id: userSender.id, version: userSender.version },
+        data: {
+          balance: {
+            decrement: amount,
+          },
+          version: {
+            increment: 1,
+          },
+        },
+      });
+      if (!updatedUserSender) {
+        throw new BadRequestException('Optimistic lock conflict');
+      }
+      await tx.user.update({
+        where: { id: receiverId, version: receiver.version },
+        data: {
+          balance: {
+            increment: amount,
+          },
+        },
+      });
+      const operation = await tx.operation.create({
+        data: {
+          amount,
+          senderId,
+          receiverId,
+          status: OperationStatus.SUCCESS,
+        },
+      });
+      await this.cacheManager.del(`/user/${senderId}/balance`);
+      await this.cacheManager.del(`/user/${receiverId}/balance`);
+      return operation;
+    });
   }
 }
